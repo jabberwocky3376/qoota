1750aee59e7437469410678c83e88fa4ca095da8
akutagawa.junki <akutagawa.junki@ditgroup.jp>
2020-03-10 15:59:14 +0900

    カラーを変更

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
+import colors from 'vuetify/es5/util/colors';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+    iconfont: 'md',
+    theme: {
+        primary: colors.indigo.base,
+        secondary: colors.purple.base,
+        accent: colors.cyan.base,
+        error: colors.red.base,
+        warning: colors.orange.base,
+        info: colors.lightBlue.base,
+        success: colors.green.base,
+    },
+});
 
 export default new Vuetify({
 });
